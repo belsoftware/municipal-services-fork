3e6a62a540fac9cb6b52e9ba8d3e9d3e72e52a25
vipin <vipinpublicdomain@gmail.com>
2021-04-09 11:11:04 +0530

    Casting to long

--- ws-calculator/src/main/java/org/egov/wscalculation/service/DemandService.java
+++ ws-calculator/src/main/java/org/egov/wscalculation/service/DemandService.java
@@ -678,7 +678,7 @@ public class DemandService {
 	 */
 	public void generateDemandForULB(Map<String, Object> master, RequestInfo requestInfo, String tenantId) {
 		log.info("Billing master data values for non metered connection:: {}", master);
-		long startDay = (((long) master.get(WSCalculationConstant.Demand_Generate_Date_String)) / 86400000);
+		long startDay = ((Long.parseLong(master.get(WSCalculationConstant.Demand_Generate_Date_String).toString())) / 86400000);
 		if(isCurrentDateIsMatching((String) master.get(WSCalculationConstant.Billing_Cycle_String), startDay)) {
 			List<String> connectionNos = waterCalculatorDao.getConnectionsNoList(tenantId,
 					WSCalculationConstant.nonMeterdConnection);
