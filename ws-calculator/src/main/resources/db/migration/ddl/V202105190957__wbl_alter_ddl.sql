f9c19191ed24985c51b4dc5071aa0ae845cdb61d
divyachandran <chandran.divya07@gmail.com>
2021-05-21 15:57:15 +0530

    ddl for create and alter bill_failure

---
+++ ws-calculator/src/main/resources/db/migration/ddl/V202105190957__wbl_alter_ddl.sql
@@ -0,0 +1,6 @@
+ALTER TABLE eg_ws_failed_bill ALTER COLUMN lastreading DROP NOT NULL;
+ALTER TABLE eg_ws_failed_bill ALTER COLUMN fromdate DROP NOT NULL;
+ALTER TABLE eg_ws_failed_bill ALTER COLUMN currentreading DROP NOT NULL;
+ALTER TABLE eg_ws_failed_bill ALTER COLUMN todate DROP NOT NULL;
+ALTER TABLE eg_ws_failed_bill ALTER COLUMN lastreading DROP NOT NULL;
+
