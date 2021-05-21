37f46059018166f64bc377780e6b12704f9e570c
divyachandran <chandran.divya07@gmail.com>
2021-05-21 16:05:04 +0530

    Revert "ddl for create and alter bill_failure"
    This reverts commit f9c19191ed24985c51b4dc5071aa0ae845cdb61d.

--- ws-calculator/src/main/resources/db/migration/ddl/V202105190957__wbl_alter_ddl.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE eg_ws_failed_bill ALTER COLUMN lastreading DROP NOT NULL;
-ALTER TABLE eg_ws_failed_bill ALTER COLUMN fromdate DROP NOT NULL;
-ALTER TABLE eg_ws_failed_bill ALTER COLUMN currentreading DROP NOT NULL;
-ALTER TABLE eg_ws_failed_bill ALTER COLUMN todate DROP NOT NULL;
-ALTER TABLE eg_ws_failed_bill ALTER COLUMN lastreading DROP NOT NULL;
-
